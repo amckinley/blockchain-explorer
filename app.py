fe3b051df1d2f6cf4805c85607d2dbce9ff91976
Austin McKinley <bearontheroof@gmail.com>
2024-09-23 16:17:12 -0700

    Got it working

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import warnings
-warnings.filterwarnings("ignore")
-
 import os
-from web3 import Web3, exceptions
-
+from dotenv import load_dotenv
+from web3 import Web3
 from flask import Flask, jsonify
 
+warnings.filterwarnings("ignore")
+load_dotenv()
+
 api_key = os.getenv('API_KEY')
-infura_url =  'https://mainnet.infura.io/v3/{}'.format(api_key)
+infura_url = f'https://mainnet.infura.io/v3/{api_key}'
 web3 = Web3(Web3.HTTPProvider(infura_url))
 
 app = Flask(__name__)
@@ -16,11 +17,14 @@ app = Flask(__name__)
 def index():
     return jsonify({"message": "Hello from AWS Lambda!"})
 
-@app.route('/address/balance')
+@app.route('/address/balance', methods=['GET'])
 def balance():
-    bal = web3.eth.get_balance("0xc94770007dda54cF92009BFF0dE90c06F603a09f")
-    return jsonify({"balance": web3.fromWei(bal, 'ether')})
+    # Fetch Ethereum balance for a hardcoded address
+    balance = web3.eth.get_balance("0xc94770007dda54cF92009BFF0dE90c06F603a09f")
 
+    # Use web3.fromWei to convert from Wei to Ether
+    balance_in_ether = web3.from_wei(balance, 'ether')
+    return jsonify({"balance": str(balance_in_ether)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5005, debug=True)
--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,5 @@
 from app import app
+from mangum import Mangum
 
-def handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': app.test_client().get('/').data.decode('utf-8'),
-        'headers': {
-            'Content-Type': 'application/json'
-        }
-    }
+# Mangum wraps the Flask app for use with AWS Lambda
+handler = Mangum(app)
